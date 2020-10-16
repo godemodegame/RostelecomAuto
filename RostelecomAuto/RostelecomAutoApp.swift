3279b957355682bb4b5250f37484f6d1b2f02251
Кирилл Кириленко <k.kirilenko@lucky.online>
2020-10-16 22:14:29 +0300

    Initial Commit

---
+++ RostelecomAuto/RostelecomAutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RostelecomAutoApp.swift
+//  RostelecomAuto
+//
+//  Created by godemodegame on 16.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct RostelecomAutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
